fa7b3c885b6c7600bc4ac4acdf52ac32c0189a90
Vaal-Fegor <81292626+Vaal-Fegor@users.noreply.github.com>
2021-09-23 22:29:57 +0300

    Add files via upload

--- routers/userRoute.js
+++ routers/userRoute.js
@@ -1,1 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const userController = require('../controllers/userController')
 
+router.get('/',userController.index)
+router.post('/create',userController.create)
+router.post('/getById',userController.getById)
+router.post('/getByName',userController.getByName)
+router.post('/update',userController.updateById)
+router.post('/delete',userController.deleteById)
+
+module.exports = router
