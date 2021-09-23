644ffb1ec72a982843c187ad642a24b4804f221d
Vaal-Fegor <81292626+Vaal-Fegor@users.noreply.github.com>
2021-09-23 22:28:38 +0300

    Add files via upload

--- models/users.js
+++ models/users.js
@@ -1,1 +1,29 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    id: mongoose.ObjectId,
+    name: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    deleted: {
+        type: Boolean,
+        default: false
+    },
+    deletedAt: {
+        type: Date,
+        //default: " "
+    }
+} , {timestamps: true})
+
+
+const user = mongoose.model('user', userSchema)
+module.exports = user
+
 
