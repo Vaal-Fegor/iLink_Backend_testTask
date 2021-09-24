f24a73b77a150bf5ebc06680f6d5ffa3b0102451
Vaal-Fegor <81292626+Vaal-Fegor@users.noreply.github.com>
2021-09-24 14:51:11 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+require('dotenv').config()
 
 const userRouter = require('./routers/userRoute')
 
@@ -22,7 +23,7 @@ app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}!`)
